858d7c147c92d21c9beaba953512fc2eb506e3ee
Alan <alanmannon@gmail.com>
2021-05-06 20:40:53 -0500

    added state abbreviations and code to schema. Running db:seed will now populate the database correctly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_014301) do
+ActiveRecord::Schema.define(version: 2021_05_07_013436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_04_27_014301) do
     t.float "adults_with_hs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "abbreviation"
+    t.string "code"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@ state_data_csv = CSV.parse(state_data, :headers => true, :encoding => "ISO-8859-
 state_data_csv.each do |row|
   s = State.new
   s.state = row["state"]
-  s.median_income = row["median_income"]
-  s.unemployment = row["unemployment"]
-  s.metro_population = row["metro_population"]
-  s.adults_with_hs = row["adults_with_hs"]
-  s.save
+  s.median_income = row["median_household_income"]
+  s.unemployment = row["share_unemployed_seasonal"]
+  s.metro_population = row["share_population_in_metro_areas"]
+  s.adults_with_hs = row["share_population_with_high_school_degree"]
+  s.save!
 end
 
-puts "#{State.count} states added to DB"
+# puts "#{State.count} states added to DB"
 
-# abbrev_text = File.read(Rails.root.join("lib", "seeds", "abbreviations.csv"))
-# abbrev_csv = CSV.parse(abbrev_text, headers: :first_row, :liberal_parsing => true, :encoding => "ISO-8859-1")
-# abbrev_csv.each do |row|
-#   puts row.to_hash
-# end
+abbrev_text = File.read(Rails.root.join("lib", "seeds", "abbreviations.csv"))
+abbrev_csv = CSV.parse(abbrev_text, :headers => true, :encoding => "UTF-8", liberal_parsing: true)
+abbrev_csv.each do |row|
+  state = abbrev_csv.headers[0]
+  s = State.find_by(state: [row[state]])
+  s.abbreviation = row["Abbrev"]
+  s.code = row["Code"]
+  s.save!
+end
